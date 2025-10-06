1d511bdaa6a02dfc27ec798be4bfc03912c86518
minwoo <minals0520@naver.com>
2025-10-06 22:33:16 +0900

    add: Exception filter 추가

--- nestjs/project/src/cats/cats.controller.ts
+++ nestjs/project/src/cats/cats.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter) // 모든 api에서 사용할 거라면 class 상단에 데코레이터를 추가
   getAllCat() {
+    throw new HttpException('api dose not working', 401);
     return 'all cat';
   }
 
