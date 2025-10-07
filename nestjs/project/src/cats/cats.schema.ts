971ed2ce2ef2400ca698c851e338060db35b1e48
minwoo <minals0520@naver.com>
2025-10-07 21:23:20 +0900

    refactor: DTO 패턴 적용, response 필터링

--- nestjs/project/src/cats/cats.schema.ts
+++ nestjs/project/src/cats/cats.schema.ts
@@ -1,11 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { HydratedDocument, SchemaOptions } from 'mongoose';
 
 export type CatDocument = HydratedDocument<Cat>;
 
 const options: SchemaOptions = {
   timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
 };
 
 @Schema(options)
@@ -14,27 +15,30 @@ export class Cat {
     required: true,
     unique: true,
   })
-  @IsEmail()
-  @IsNotEmpty()
   email: string;
 
   @Prop({
     required: true,
   })
-  @IsString()
-  @IsNotEmpty()
   name: string;
 
   @Prop({
     required: true,
   })
-  @IsString()
-  @IsNotEmpty()
   password: string;
 
-  @Prop() // default로 required false이므로 지정하지 않음
-  @IsString()
+  @Prop() // default가 required false이므로 지정하지 않음
   imgUrl: string;
+
+  readonly readOnlyData: { id: string; email: string; name: string };
 }
 
 export const CatSchema = SchemaFactory.createForClass(Cat);
+
+CatSchema.virtual('readOnlyData').get(function (this: CatDocument) {
+  return {
+    id: this._id.toString(),
+    email: this.email,
+    name: this.name,
+  };
+});
