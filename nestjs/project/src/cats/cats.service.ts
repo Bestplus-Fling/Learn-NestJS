61c9272d9402230051114c1956459fda8987a3fd
minwoo <minals0520@naver.com>
2025-10-08 22:51:30 +0900

    refactor: Repository 패턴 적용 - cats.repository.ts 생성 -> service에서 db 접근 로직을 분리

--- nestjs/project/src/cats/cats.service.ts
+++ nestjs/project/src/cats/cats.service.ts
@@ -1,16 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CatRequestDto } from './dto/cats.request.dto';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Cat } from './cats.schema';
 import * as bcrypt from 'bcrypt';
+import { CatsRepository } from './cats.repository';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private catModel: Model<Cat>) {}
+  constructor(private readonly catsRepository: CatsRepository) {}
   async signUp(body: CatRequestDto) {
     const { email, name, password } = body;
-    const isCatExist = await this.catModel.exists({ email });
+    const isCatExist = await this.catsRepository.existsByEmail(email);
 
     if (isCatExist) {
       throw new UnauthorizedException('해당 고양이는 이미 존재합니다.'); // 같은 내용
@@ -20,7 +18,7 @@ export class CatsService {
     // 비밀번호 암호화
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const cat = await this.catModel.create({
+    const cat = await this.catsRepository.create({
       email,
       name,
       password: hashedPassword,
