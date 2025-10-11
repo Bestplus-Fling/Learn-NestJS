5e6e4b99a5012f0ac4763d3005560cba7f5c743e
minwoo <minals0520@naver.com>
2025-10-11 20:53:11 +0900

    feat: 생명주기 구현체 작성 - 함수 생성, 게이트웨이 연결, 연결 중단시 각 메소드의 동작 순서와 lifecycle 확인

--- chattings/src/chats/chats.gateway.ts
+++ chattings/src/chats/chats.gateway.ts
@@ -1,13 +1,46 @@
+import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  /* 생성 주기
+    1. constructor(생성자)
+    2. afterInit(OnGatewayInit 이후)
+      - 게이트웨이가 실행이 될 때, 가장 먼저 실행(단, 생성자 다음)
+    3. handleConnection
+      - Gateway의 연결이 정상적으로 완료된 경우
+  */
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  afterInit() {
+    this.logger.log('init');
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    // socket.id: 소켓 아이디(자동생성) socket.nsp(네임 스페이스의).name(이름)
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+  }
+
   // front browser에서 보낸 event 이름을 추가
   @SubscribeMessage('new_user')
   handleNewUser(
