989f8648ad9f1eedbeaa665924a6ecd78cf5b75d
bestplus <minals0520@naver.com>
2025-12-12 21:51:08 +0900

    feat: 비밀번호 암호화하기 - bcrypt를 이용해 salt를 생성하고, 비밀번호 해시화 후 저장

--- nestjs-board-app/src/auth/user.repository.ts
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -6,6 +6,7 @@ import {
 import { AuthCredentialDto } from 'src/auth/dto/auth-credential.dto';
 import { User } from 'src/auth/user.entity';
 import { DataSource, Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -14,7 +15,10 @@ export class UserRepository extends Repository<User> {
   }
 
   async createUser({ username, password }: AuthCredentialDto): Promise<void> {
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {
