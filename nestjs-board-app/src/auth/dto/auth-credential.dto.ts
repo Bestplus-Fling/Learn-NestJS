f97aecff1f5e0c9e149c64ef8f67abb0be963ec4
bestplus <minals0520@naver.com>
2025-12-12 21:23:07 +0900

    feat: username 유니크 설정 - Unique(['username'])을 통해 unique한지 검증하는 단계 추가

--- nestjs-board-app/src/auth/dto/auth-credential.dto.ts
+++ nestjs-board-app/src/auth/dto/auth-credential.dto.ts
@@ -9,7 +9,7 @@ export class AuthCredentialDto {
   @IsString()
   @MinLength(4)
   @MaxLength(20)
-  @Matches(/^[a-zA-Z0-9]8$/, {
+  @Matches(/^[a-zA-Z0-9]*$/, {
     message: 'password only accepts english and number',
   }) // 영어랑 숫자만 가능하게 유효성 검사
   password: string;
--- nestjs-board-app/src/auth/user.repository.ts
+++ nestjs-board-app/src/auth/user.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { AuthCredentialDto } from 'src/auth/dto/auth-credential.dto';
 import { User } from 'src/auth/user.entity';
 import { DataSource, Repository } from 'typeorm';
@@ -11,7 +15,14 @@ export class UserRepository extends Repository<User> {
 
   async createUser({ username, password }: AuthCredentialDto): Promise<void> {
     const user = this.create({ username, password });
-
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
